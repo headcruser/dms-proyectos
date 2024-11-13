08068da90fa24e4d915c0220a67b31c4412c0f10
Daniel Martinez Sierra <headcruser@gmail.com>
2024-11-12 20:49:57 -0600

    Inicio: Se cambia pantalla de inicio

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,14 +8,14 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" />
         <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900&display=swap" rel="stylesheet" />
 
-        <meta name="description" content="Bienvenid@ a fuego bites">
+        <meta name="description" content="Bienvenid@ a dms jales">
         <meta name="theme-color" content="#D7033D">
         <meta name="twitter:card" content="summary">
         <meta name="theme-color" content="#D7033D">
         <meta name="twitter:card" content="summary">
         <meta property="og:type" content="website">
-        <meta property="og:title" name="twitter:title" content="Fuego Bites - Date un gusto ">
-        <meta property="og:description" name="twitter:description" content="Bienvenid@ a fuego bites">
+        <meta property="og:title" name="twitter:title" content="DMS Jales ">
+        <meta property="og:description" name="twitter:description" content="Bienvenid@ a DMS Jales">
 
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
@@ -26,7 +26,85 @@
         {{-- @vite('resources/js/app.css') --}}
     </head>
     <body class="bg-light">
-        <header class="mb-7">
+        <!-- Section: Design Block -->
+        <section class="mb-10">
+        <!-- Navbar -->
+        <nav class="navbar navbar-expand-lg navbar-light bg-white shadow-2">
+            <!-- Container wrapper -->
+            <div class="container-fluid">
+            <div class="d-flex">
+                <!-- Toggle button -->
+                <button
+                        class="navbar-toggler"
+                        type="button"
+                        data-mdb-collapse-init
+                        data-mdb-target="#navbarSupportedContent"
+                        aria-controls="navbarSupportedContent"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                        >
+                <i class="fas fa-bars"></i>
+                </button>
+
+                <!-- Navbar brand -->
+                <a class="navbar-brand ms-3" href="#">
+                    <img src="{{ asset('img/logo.png') }}" width="50px" height="50px" />
+                </a>
+            </div>
+
+            <!-- Collapsible wrapper -->
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <!-- Left links -->
+                <ul
+                    class="navbar-nav me-auto mb-2 ms-2 ps-1 ms-lg-0 ps-lg-0 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Proyectos</a>
+                    </li>
+                </ul>
+                <!-- Left links -->
+            </div>
+            <!-- Collapsible wrapper -->
+
+            <!-- Right elements -->
+            <div class="d-flex align-items-center">
+                <a href="{{ route('login') }}" data-mdb-ripple-init class="btn btn-link px-3 mb-1 me-2">Iniciar sesión</a>
+            </div>
+            <!-- Right elements -->
+            </div>
+            <!-- Container wrapper -->
+        </nav>
+        <!-- Navbar -->
+
+        <!-- Background image -->
+        <div
+            class="p-5 text-center bg-image"
+            style="
+                    background-image: url('https://mdbootstrap.com/img/new/textures/full/142.jpg');
+                    height: 500px;
+                    "
+            ></div>
+        <!-- Background image -->
+
+        <div class="container">
+            <div
+                class="card mx-4 mx-md-5 text-center shadow-5-strong"
+                style="
+                        margin-top: -170px;
+                        background: hsla(0, 0%, 100%, 0.7);
+                        backdrop-filter: blur(30px);
+                        "
+                >
+            <div class="card-body px-4 py-5 px-lg-3">
+                <h1 class="display-3 fw-bold ls-tight mb-4">
+                Desarrollos a la medida <br />
+                <span class="text-primary">para tu negocio</span>
+                </h1>
+            </div>
+            </div>
+        </div>
+        </section>
+<!-- Section: Design Block -->
+        {{-- <header class="mb-7">
           <!-- Background image -->
           <div id="intro" class="bg-image shadow-1-strong"
               style="background-image: url({{ asset('img/panditas.png') }});height: 500px;" alt="Daniela Slezáková">
@@ -163,6 +241,6 @@
           </div>
           <!-- Copyright -->
         </footer>
-        <!-- Footer -->
+        <!-- Footer --> --}}
     </body>
 </html>

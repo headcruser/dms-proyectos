bc757453934466deef2114467b4fed2c0169e483
Daniel Martinez Sierra <headcruser@gmail.com>
2026-03-22 13:30:44 -0600

    Update to laravel 12 and fix image

--- resources/js/Pages/Admin/User/Edit.vue
+++ resources/js/Pages/Admin/User/Edit.vue
@@ -53,105 +53,107 @@ const submit = () => {
 <template>
     <AuthenticatedLayout>
         <div class="mt-4 mx-4">
-            <MDBContainer>
+            <MDBContainer fluid>
                 <!--Section: Content-->
                 <section>
                     <MDBRow>
-                    <MDBCol md="12" class="mb-4 mb-md-0">
-                        <MDBCard class="mb-4">
-                        <MDBCardHeader class="py-3">
-                            <strong>Editar usuario</strong>
-                        </MDBCardHeader>
-                        <MDBCardBody>
-                            <form @submit.prevent="submit">
-                                <MDBRow>
-                                    <MDBCol class="col-12 col-sm-6">
-                                        <div class="mb-3">
-                                            <strong>Foto</strong>
-                                        </div>
+                        <MDBCol md="12" class="mb-4 mb-md-0">
+                            <MDBCard>
+                                <MDBCardHeader>
+                                    <strong>Editar usuario</strong>
+                                </MDBCardHeader>
+                                <MDBCardBody>
+                                    <form @submit.prevent="submit">
+                                        <MDBRow>
+                                            <MDBCol class="col-12 col-sm-6">
+                                                <div class="mb-3">
+                                                    <fieldset>
+                                                        <legend>Información del usuario</legend>
+                                                    </fieldset>
+                                                </div>
 
-                                        <div class="d-flex justify-content-center">
-                                            <img
-                                                v-if="user.photo"
-                                                class="rounded-circle shadow-1 mb-3"
-                                                :src="`/storage/users/${user.id}/${user.photo}`"
-                                                alt="avatar"
-                                                style="width: 50px"
-                                            />
-                                        </div>
+                                                <div class="d-flex justify-content-center">
+                                                    <img
+                                                        v-if="user.photo"
+                                                        class="rounded-circle shadow-1 mb-3"
+                                                        :src="`/storage/users/${user.id}/${user.photo}`"
+                                                        alt="avatar"
+                                                        style="width: 50px"
+                                                    />
+                                                </div>
 
-                                        <div class="d-flex justify-content-center mb-4 border-1">
-                                            <MDBFileUpload
-                                                :defaultFiles="[
-                                                ]"
-                                                defaultMsg="Arrastre y suelte un archivo"
-                                                class="shadow-1"
-                                                disabledRemoveBtn
-                                                @input="form.photo = $event.target.files[0]"
-                                            />
-                                        </div>
+                                                <div class="d-flex justify-content-center mb-4 border-1">
+                                                    <MDBFileUpload
+                                                        :defaultFiles="[
+                                                        ]"
+                                                        defaultMsg="Arrastre y suelte un archivo"
+                                                        class="shadow-1"
+                                                        disabledRemoveBtn
+                                                        @input="form.photo = $event.target.files[0]"
+                                                    />
+                                                </div>
 
-                                        <div class="form-group pb-3">
-                                            <MDBInput
-                                                type="text"
-                                                v-model="form.name"
-                                                label="Nombre"
-                                                :class="{'is-invalid':form.errors.name}"
-                                                :invalid-feedback="form.errors.name"
-                                            />
-                                        </div>
+                                                <div class="form-group pb-3">
+                                                    <MDBInput
+                                                        type="text"
+                                                        v-model="form.name"
+                                                        label="Nombre"
+                                                        :class="{'is-invalid':form.errors.name}"
+                                                        :invalid-feedback="form.errors.name"
+                                                    />
+                                                </div>
 
-                                        <div class="form-group pb-3">
-                                            <MDBInput
-                                                type="email"
-                                                v-model="form.email"
-                                                label="Correo"
-                                                :class="{'is-invalid':form.errors.email}"
-                                                :invalid-feedback="form.errors.email"
-                                            />
-                                        </div>
+                                                <div class="form-group pb-3">
+                                                    <MDBInput
+                                                        type="email"
+                                                        v-model="form.email"
+                                                        label="Correo"
+                                                        :class="{'is-invalid':form.errors.email}"
+                                                        :invalid-feedback="form.errors.email"
+                                                    />
+                                                </div>
 
-                                        <div class="form-group pb-5">
-                                            <MDBInput
-                                                type="password"
-                                                v-model="form.password"
-                                                label="Contraseña"
-                                                aria-autocomplete="off"
-                                                helper="Escribir solo si deseas cambiar la contraseña"
-                                                :class="{'is-invalid':form.errors.name}"
-                                                :invalid-feedback="form.errors.name"
-                                            />
-                                        </div>
+                                                <div class="form-group pb-5">
+                                                    <MDBInput
+                                                        type="password"
+                                                        v-model="form.password"
+                                                        label="Contraseña"
+                                                        aria-autocomplete="off"
+                                                        helper="Escribir solo si deseas cambiar la contraseña"
+                                                        :class="{'is-invalid':form.errors.name}"
+                                                        :invalid-feedback="form.errors.name"
+                                                    />
+                                                </div>
 
-                                        <div class="form-group mb-3">
-                                            <MDBCheckbox :label="'Enviar datos por correo'" v-model="form.enviar_datos"/>
-                                        </div>
+                                                <div class="form-group mb-3">
+                                                    <MDBCheckbox :label="'Enviar datos por correo'" v-model="form.enviar_datos"/>
+                                                </div>
 
-                                        <MDBBtn
-                                          color="primary"
-                                          block
-                                          class="mb-2"
-                                          type="submit"
-                                          :disabled="form.processing"> Guardar </MDBBtn>
+                                                <MDBBtn
+                                                color="primary"
+                                                block
+                                                class="mb-2"
+                                                type="submit"
+                                                :disabled="form.processing"> Guardar </MDBBtn>
 
-                                    </MDBCol>
+                                            </MDBCol>
 
-                                    <MDBCol class="col-12 col-sm-6">
-                                        <div class="mb-3">
-                                            <strong>Roles</strong>
-                                        </div>
-                                        <div>
-                                            <template v-for="(option,key) in checkedValues" :key="key">
-                                                <MDBCheckbox :label="option.label" :value="option.id" v-model="option.checked"/>
-                                            </template>
-                                        </div>
-                                    </MDBCol>
-                                </MDBRow>
-                            </form>
-                        </MDBCardBody>
-                        </MDBCard>
+                                            <MDBCol class="col-12 col-sm-6">
+                                                <div class="mb-3">
+                                                    <strong>Roles</strong>
+                                                </div>
+                                                <div>
+                                                    <template v-for="(option,key) in checkedValues" :key="key">
+                                                        <MDBCheckbox :label="option.label" :value="option.id" v-model="option.checked"/>
+                                                    </template>
+                                                </div>
+                                            </MDBCol>
+                                        </MDBRow>
+                                    </form>
+                                </MDBCardBody>
+                            </MDBCard>
 
-                    </MDBCol>
+                        </MDBCol>
                     </MDBRow>
                 </section>
                 <!--Section: Content-->
